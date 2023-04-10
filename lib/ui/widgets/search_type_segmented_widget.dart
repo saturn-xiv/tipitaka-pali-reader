c62d0e4a463b5c8aedf15d76b952df6ea08e147a
pndaza <mrpndaza@outlook.com>
2023-04-10 15:44:14 +0630

    add distance mode config

--- lib/ui/widgets/search_type_segmented_widget.dart
+++ lib/ui/widgets/search_type_segmented_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:tipitaka_pali/ui/screens/home/search_page/easy_number_input.dart';
 import '../../../../services/prefs.dart';
 import '../../../../utils/pali_script.dart';
 import '../../../../utils/pali_script_converter.dart';
@@ -36,55 +37,98 @@ class _SearchTypeSegmentedControlState
     3: 'any part',
   };
 
+  late bool isDistanceMoe;
+
+  @override
+  void initState() {
+    super.initState();
+    isDistanceMoe = _selectedIndex == 2;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Theme.of(context).dividerColor,
-          width: 1.0,
-        ),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: _segmentValues.entries.map((entry) {
-            return Expanded(
-              child: InkWell(
-                onTap: () {
-                  setState(() {
-                    _selectedIndex = entry.key;
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Theme.of(context).dividerColor,
+              width: 1.0,
+            ),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: _segmentValues.entries.map((entry) {
+                return Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        _selectedIndex = entry.key;
 
-                    widget.onModeChanged(getQueryModeFromSelection(entry.key));
-                  });
-                },
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  decoration: BoxDecoration(
-                    color: _selectedIndex == entry.key
-                        ? Theme.of(context).colorScheme.secondary
-                        : Colors.transparent,
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Center(
-                    child: Text(
-                      entry.value,
-                      style: TextStyle(
+                        isDistanceMoe = _selectedIndex == 2;
+
+                        widget.onModeChanged(
+                            getQueryModeFromSelection(entry.key));
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      decoration: BoxDecoration(
                         color: _selectedIndex == entry.key
-                            ? Colors.white
-                            : Theme.of(context).textTheme.bodyLarge?.color,
-                        fontWeight: FontWeight.bold,
+                            ? Theme.of(context).colorScheme.secondary
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          entry.value,
+                          style: TextStyle(
+                            color: _selectedIndex == entry.key
+                                ? Colors.white
+                                : Theme.of(context).textTheme.bodyLarge?.color,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            );
-          }).toList(),
+                );
+              }).toList(),
+            ),
+          ),
         ),
-      ),
+        // extra setting for distance
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 200),
+          transitionBuilder: (child, animation) {
+            return SizeTransition(
+              sizeFactor: animation,
+              axis: Axis.vertical,
+              child: child,
+            );
+          },
+          child: isDistanceMoe
+              ? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text('Distance between words'),
+                      const Spacer(),
+                      EasyNumberInput(
+                          initial: widget.wordDistance,
+                          onChanged: (value) {
+                            widget.onDistanceChanged(value);
+                          }),
+                      // right margin
+                    ],
+                  ),
+                )
+              : const SizedBox.shrink(),
+        ),
+      ],
     );
   }
 
