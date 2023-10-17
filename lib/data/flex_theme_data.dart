989a6f8aa46f3a63a41bd5a7078088ee0800281c
Rydmike <39990307+rydmike@users.noreply.github.com>
2023-10-17 04:58:33 +0300

    Remove commented code and add tip about custom colors

--- lib/data/flex_theme_data.dart
+++ lib/data/flex_theme_data.dart
@@ -1,32 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
-
-FlexSchemeColor myFlexColorLight = FlexSchemeColor.from(
-  primary: Colors.brown,
-);
-
-/*
-const FlexSchemeColor myScheme1Light = FlexSchemeColor(
-  primary: Color(0xFF4E0028),
-  primaryVariant: Color(0xFF320019),
-  secondary: Color(0xFF003419),
-  secondaryVariant: Color(0xFF002411),
-  // The built in schemes use their secondary variant color as their
-  // custom app bar color, it could of course be any color, but for consistency
-  // we will do the same in this custom FlexSchemeColor.
-  appBarColor: Color(0xFF002411),
-);
-
-const FlexSchemeColor myScheme1Dark = FlexSchemeColor(
-  primary: Color(0xFF9E7389),
-  primaryVariant: Color(0xFF775C69),
-  secondary: Color(0xFF738F81),
-  secondaryVariant: Color(0xFF5C7267),
-  // Again we use same secondaryVariant color as optional custom app bar color.
-  appBarColor: Color(0xFF5C7267),
-);
-
-*/
+import 'package:flutter/material.dart';
 
 class MyThemes {
   static const orange2Name = 'Orange 2';
@@ -180,6 +153,11 @@ final List<FlexSchemeData> myFlexSchemes = <FlexSchemeData>[
       tertiaryContainer: Color(0xFF535393),
     ),
   ),
+
+  // @rydmike: DESIGN TIP: You cannot use same primary secondary colors in
+  // light and dark mode, it creates themes with poor contrast in light or dark
+  // mode depending on selected colors. The colors needs to be tunes for
+  // each mode to look good.
   const FlexSchemeData(
     name: 'Pa-Auk Burgundy',
     description: 'Paauka Color Burgundy',
