d1365dac82cdebbd74e4a9cad91a223a0eb671bc
bksubhuti <bksubhuti@gmail.com>
2024-02-08 16:53:48 +0530

    increment db version

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -2,7 +2,7 @@ const String highlightTagName = 'highlight';
 
 class DatabaseInfo {
   DatabaseInfo._();
-  static const int version = 42;
+  static const int version = 43;
   static const String fileName = 'tipitaka_pali.db';
 }
 
